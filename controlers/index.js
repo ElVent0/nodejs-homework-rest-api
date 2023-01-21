baefabe92bb2e6b7d19462fe316e9615044b33a0
Vasyl Tarasyuk <vasfreelan@gmail.com>
2023-01-21 14:11:21 +0200

    done

---
+++ controlers/index.js
@@ -0,0 +1,98 @@
+const { Contact } = require("../service/index");
+const { Error } = require("../middleWare/index");
+
+const getAll = async (req, res) => {
+  const result = await Contact.find({});
+
+  res.json({
+    status: "done",
+    code: 200,
+    data: { result },
+  });
+};
+
+const getById = async (req, res) => {
+  const { id } = req.params;
+  const result = await Contact.findById(id);
+
+  if (!result) {
+    throw new Error(404, "Not found");
+  }
+
+  res.json({
+    status: "done",
+    code: 200,
+    data: { result },
+  });
+};
+
+const add = async (req, res) => {
+  const result = await Contact.create(req.body);
+  res.status(201).json({ status: "done", code: 201, data: { result } });
+};
+
+const removeById = async (req, res) => {
+  const { id } = req.params;
+  const result = await Contact.findByIdAndRemove(id);
+
+  if (!result) {
+    throw new Error(404, "Not found");
+  }
+
+  res.json({
+    status: "done",
+    code: 200,
+    data: { result },
+  });
+};
+
+const updateById = async (req, res) => {
+  const { id } = req.params;
+  const result = await Contact.findByIdAndUpdate(id, req.body, {
+    new: true,
+  });
+
+  if (!result) {
+    throw new Error(404, "Not found");
+  }
+
+  res.json({
+    status: "done",
+    code: 200,
+    data: { result },
+  });
+};
+
+const updateFavorite = async (req, res) => {
+  const { id } = req.params;
+  const { favorite } = req.body;
+
+  if (!favorite) {
+    throw new Error(400, `Missing field favorite`);
+  }
+
+  const result = await Contact.findByIdAndUpdate(
+    id,
+    { favorite },
+    { new: true }
+  );
+
+  if (!result) {
+    throw new Error(404, "Not found");
+  }
+
+  res.json({
+    status: "done",
+    code: 200,
+    data: { result },
+  });
+};
+
+module.exports = {
+  getAll,
+  add,
+  getById,
+  removeById,
+  updateById,
+  updateFavorite,
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,90 +1,26 @@
 const express = require("express");
+const { wrapper, validator } = require("../../middleWare/index");
+const {
+  getAll,
+  getById,
+  add,
+  removeById,
+  updateById,
+  updateFavorite,
+} = require("../../controlers/index");
+const { joi, favoriteJoi } = require("../../service/index");
+
 const router = express.Router();
-const Joi = require("joi");
-const contactsHandler = require("../../models/contacts");
-const { NotFound, BadRequest, InternalServerError } = require("http-errors");
 
-const schema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.number().required(),
-});
-
-router.get("/", async (req, res, next) => {
-  try {
-    const result = await contactsHandler.listContacts();
-    res.json({ status: "Done", code: 200, data: { result } });
-  } catch (e) {
-    throw new InternalServerError(`Error`);
-  }
-});
-
-router.get("/:contactId", async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await contactsHandler.getContactById(contactId);
-
-    if (!result) {
-      throw new NotFound(`Not found`);
-    }
-
-    res.json({ status: "Done", code: 200, data: { result } });
-  } catch (e) {
-    throw new InternalServerError(`Error`);
-  }
-});
-
-router.post("/", async (req, res, next) => {
-  try {
-    if (schema.validate(req.body)) {
-      throw new BadRequest("Missing required name field");
-    }
-
-    const result = await contactsHandler.addContact(req.body);
-    res.status(201).json({ status: "Done", code: 201, data: { result } });
-  } catch (e) {
-    throw new InternalServerError(`Error`);
-  }
-});
-
-router.delete("/:contactId", async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-
-    const result = await contactsHandler.removeContact(contactId);
-
-    if (!result) {
-      throw new NotFound(`Not found`);
-    }
-
-    res.status(200).json({
-      status: "Done",
-      code: 200,
-      data: { result },
-      message: "Contact deleted",
-    });
-  } catch (e) {
-    throw new InternalServerError(`Error`);
-  }
-});
-
-router.put("/:contactId", async (req, res, next) => {
-  try {
-    if (schema.validate(req.body)) {
-      throw new BadRequest("Missing fields");
-    }
-
-    const { contactId } = req.params;
-    const result = await contactsHandler.updateContact(contactId, req.body);
-
-    if (!result) {
-      throw new NotFound(`Not found`);
-    }
-
-    res.status(200).json({ status: "Done", code: 200, data: { result } });
-  } catch (e) {
-    throw new InternalServerError(`Error`);
-  }
-});
+router.get("/", wrapper(getAll));
+router.get("/:contactId", wrapper(getById));
+router.post("/", validator(joi), wrapper(add));
+router.delete("/:contactId", wrapper(removeById));
+router.put("/:contactId", validator(joi), wrapper(updateById));
+router.patch(
+  "/:contactId/favorite",
+  validator(favoriteJoi),
+  wrapper(updateFavorite)
+);
 
 module.exports = router;
