9e92bdf5439745a496847182d577c45de5dfafa5
Vasyl Tarasyuk <vasfreelan@gmail.com>
2023-01-24 15:29:13 +0200

    1.2

--- controlers/index.js
+++ controlers/index.js
@@ -4,14 +4,14 @@ const { Error } = require("../middleWare/index");
 // Функції, що спрацьовують при запитах
 
 const getAll = async (req, res) => {
-  const { _id: owner } = req.user;
-  const { page = 1, limit = 10 } = req.query;
-  const skip = (page - 1) * limit;
-  const result = await Contact.find({ owner }, "-createdAt - updatedAt", {
-    skip,
-    limit,
-  }).populate("owner", "name email");
-  // const result = await Contact.find({});
+  // const { _id: owner } = req.user;
+  // const { page = 1, limit = 10 } = req.query;
+  // const skip = (page - 1) * limit;
+  // const result = await Contact.find({ owner }, "-createdAt - updatedAt", {
+  //   skip,
+  //   limit,
+  // }).populate("owner", "name email");
+  const result = await Contact.find({});
 
   res.json({
     status: "done",
